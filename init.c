4632c664e14e12c75dc095ba9a7aad7c74a7f57a
Nathanaël Spriet <nathanael.spriet@gmail.com>
2018-09-27 13:53:25 +0200

    Add the serial connection

--- init.c
+++ init.c
@@ -27,7 +27,68 @@ void Oscillator_Init()
     OSCICN    = 0x0C;
 }
 
+// Peripheral specific initialization functions,
+// Called from the Init_Device() function
+void Port_IO_Init()
+{
+    // P0.0  -  TX0 (UART0), Open-Drain, Digital
+    // P0.1  -  RX0 (UART0), Open-Drain, Digital
+    // P0.2  -  Unassigned,  Open-Drain, Digital
+    // P0.3  -  Unassigned,  Open-Drain, Digital
+    // P0.4  -  Unassigned,  Open-Drain, Digital
+    // P0.5  -  Unassigned,  Open-Drain, Digital
+    // P0.6  -  Unassigned,  Open-Drain, Digital
+    // P0.7  -  Unassigned,  Open-Drain, Digital
 
+    // P1.0  -  Unassigned,  Open-Drain, Digital
+    // P1.1  -  Unassigned,  Open-Drain, Digital
+    // P1.2  -  Unassigned,  Open-Drain, Digital
+    // P1.3  -  Unassigned,  Open-Drain, Digital
+    // P1.4  -  Unassigned,  Open-Drain, Digital
+    // P1.5  -  Unassigned,  Open-Drain, Digital
+    // P1.6  -  Unassigned,  Open-Drain, Digital
+    // P1.7  -  Unassigned,  Open-Drain, Digital
+
+    // P2.0  -  Unassigned,  Open-Drain, Digital
+    // P2.1  -  Unassigned,  Open-Drain, Digital
+    // P2.2  -  Unassigned,  Open-Drain, Digital
+    // P2.3  -  Unassigned,  Open-Drain, Digital
+    // P2.4  -  Unassigned,  Open-Drain, Digital
+    // P2.5  -  Unassigned,  Open-Drain, Digital
+    // P2.6  -  Unassigned,  Open-Drain, Digital
+    // P2.7  -  Unassigned,  Open-Drain, Digital
+
+    // P3.0  -  Unassigned,  Open-Drain, Digital
+    // P3.1  -  Unassigned,  Open-Drain, Digital
+    // P3.2  -  Unassigned,  Open-Drain, Digital
+    // P3.3  -  Unassigned,  Open-Drain, Digital
+    // P3.4  -  Unassigned,  Open-Drain, Digital
+    // P3.5  -  Unassigned,  Open-Drain, Digital
+    // P3.6  -  Unassigned,  Open-Drain, Digital
+    // P3.7  -  Unassigned,  Open-Drain, Digital
+
+    XBR0      = 0x04;
+    XBR2      = 0x40;
+}
+
+void led_init()
+{
+	P1MDOUT |= 0x20;
+}
+
+void uart_init()
+{
+	CKCON |= 0x10;
+	TCON |= 0x40;
+	TMOD |= 0x20;
+	TMOD &= ~0xD0;
+	SCON0 |= 0x72;
+	SCON0 &= ~0x81;
+	T2CON &= ~0x30;
+	PCON |= 0x80;
+	TH1 = 111;
+	
+}
 
 //-----------------------------------------------------------------------------
 // Initialisation globale du Microcontrôleur - 
@@ -36,4 +97,7 @@ void Init_Device(void)
 {
     Reset_Sources_Init();
     Oscillator_Init();
+		Port_IO_Init();
+		led_init();
+		uart_init();
 }
--- main.c
+++ main.c
@@ -17,6 +17,8 @@
 #include <stdio.h>
 #include "init.h"
 
+void fct_tempo(int duree_us);
+
 
 sbit LED = P1^6; // Led verte embarquée sur la carte
 
@@ -29,11 +31,20 @@ void main (void)
 {
 	
   Init_Device();
-
   
   while (1)
   {
-		
+			LED = 1;
+			SBUF0 = 'a';
+			fct_tempo(32000);
+			fct_tempo(32000);
+			fct_tempo(32000);
+			fct_tempo(32000);
+		  LED = 0;
+			fct_tempo(32000);
+			fct_tempo(32000);
+			fct_tempo(32000);
+			fct_tempo(32000);
 	}	 	 
 }
 
